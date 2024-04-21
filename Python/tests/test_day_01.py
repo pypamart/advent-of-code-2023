e4fb8f7cd37c83518431527a5e1bea2f0768b4f2
Pierre-Yves PAMART <pypamart@gmail.com>
2024-04-21 21:47:02 +0200

    :sparkles: Add algorithm to solve day01 part2 challenge

--- Python/tests/test_day_01.py
+++ Python/tests/test_day_01.py
@@ -1,18 +1,47 @@
 from day_01 import extract_calibration_value_from
 
-EXAMPLE1 = "1abc2"
-EXAMPLE2 = "pqr3stu8vwx"
-EXAMPLE3 = "a1b2c3d4e5f"
-EXAMPLE4 = "treb7uchet"
+DATA_TEST = {
+    "part1": {"1abc2": 12, "pqr3stu8vwx": 38, "a1b2c3d4e5f": 15, "treb7uchet": 77},
+    "part2": {
+        "two1nine": 29,
+        "eightwothree": 83,
+        "abcone2threexyz": 13,
+        "xtwone3four": 24,
+        "4nineeightseven2": 42,
+        "zoneight234": 14,
+        "7pqrstsixteen": 76,
+    },
+}
 
-def test_number_in_example1_is_12():
-    assert extract_calibration_value_from(EXAMPLE1) == 12
+PATTERNS_TABLE = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
 
-def test_number_in_example2_is_38():
-    assert extract_calibration_value_from(EXAMPLE2) == 38
 
-def test_number_in_example3_is_15():
-    assert extract_calibration_value_from(EXAMPLE3) == 15
+def test_extract_calibration_value_from_text_with_digit():
+    for text, expected in DATA_TEST["part1"].items():
+        assert extract_calibration_value_from(text, PATTERNS_TABLE) == expected
 
-def test_number_in_example4_is_77():
-    assert extract_calibration_value_from(EXAMPLE4) == 77
+
+def test_extract_calibration_value_from_text_with_written_number():
+    for text, expected in DATA_TEST["part2"].items():
+        assert extract_calibration_value_from(text, PATTERNS_TABLE) == expected
