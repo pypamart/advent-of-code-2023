a2b2991ff129f644cd53d8425fed08d44daffef7
Pierre-Yves PAMART <pypamart@gmail.com>
2024-04-21 21:47:02 +0200

    :sparkles: Add algorithm to solve day01 part2 challenge

--- Python/day_01/day_01.py
+++ Python/day_01/day_01.py
@@ -1,27 +1,70 @@
 from pathlib import Path
+import json
 
 INPUT_FILEPATH = Path("input.txt")
+PATTERNS_FILEPATH = Path("patterns.json")
 
 
-def extract_calibration_value_from(text: str) -> int:
-    digits = remove_letters(text)
-    first_digit = digits[0]
-    last_digit = digits[-1]
-    return int(first_digit + last_digit)
+def read_patterns_from(filepath: Path, tablename: str) -> dict:
+    with open(filepath, "r") as file:
+        patterns = json.load(file)
+    return patterns[tablename]
 
 
-def remove_letters(text: str) -> str:
-    return "".join([letter for letter in text if letter.isdigit()])
+def extract_calibration_value_from(text: str, patterns_table: dict) -> int:
+    first_digit = search_first_digit(text, patterns_table)
+    last_digit = search_last_digit(text, patterns_table)
+    return first_digit * 10 + last_digit
+
+
+def search_first_digit(text: str, patterns_table: dict) -> int:
+    first_digit = None
+    for i in range(len(text)):
+        for pattern, value in patterns_table.items():
+            if text[i:].startswith(pattern):
+                first_digit = value
+                break
+        if first_digit:
+            break
+    return first_digit
+
+
+def search_last_digit(text: str, patterns_table: dict) -> int:
+    last_digit = None
+    for i in range(len(text)):
+        for pattern, value in patterns_table.items():
+            if (i == 0 and text.endswith(pattern)) or text[:-i].endswith(pattern):
+                last_digit = value
+                break
+        if last_digit:
+            break
+    return last_digit
+
+
+def extract_sum_of_all_calibration_values_from(
+    filepath: Path, patterns_table: dict
+) -> int:
+    with open(filepath, "r") as file:
+        input_lines = file.readlines()
+
+    calibration_values = [
+        extract_calibration_value_from(line.strip(), patterns_table)
+        for line in input_lines
+    ]
+
+    return sum(calibration_values)
 
 
 if __name__ == "__main__":
 
-    with open(INPUT_FILEPATH, "r") as file:
-        input_lines = file.readlines()
+    patterns_table = read_patterns_from(PATTERNS_FILEPATH, "part1")
+    part1_result = extract_sum_of_all_calibration_values_from(
+        INPUT_FILEPATH, patterns_table
+    )
+    print(f"Part 1: {part1_result}")
 
-    calibration_values = [
-        extract_calibration_value_from(line.strip()) 
-        for line in input_lines]
-    
-    print(sum(calibration_values))
-
+    patterns_table = read_patterns_from(PATTERNS_FILEPATH, "part2")
+    part2_result = extract_sum_of_all_calibration_values_from(
+        INPUT_FILEPATH, patterns_table
+    )
+    print(f"Part 2: {part2_result}")
